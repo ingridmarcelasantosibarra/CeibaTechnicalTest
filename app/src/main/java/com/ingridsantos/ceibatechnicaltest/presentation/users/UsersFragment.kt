57600095f41a5d2370e67673e7dbfb18a1a2b499
ingridmarcelasantosibarra <marcesntos@gmail.com>
2022-07-09 09:44:04 -0500

    Add design view list users

--- app/src/main/java/com/ingridsantos/ceibatechnicaltest/presentation/users/UsersFragment.kt
+++ app/src/main/java/com/ingridsantos/ceibatechnicaltest/presentation/users/UsersFragment.kt
@@ -1,39 +1,38 @@
-package com.ingridsantos.ceibatechnicaltest
+package com.ingridsantos.ceibatechnicaltest.presentation.users
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.ingridsantos.ceibatechnicaltest.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import com.ingridsantos.ceibatechnicaltest.databinding.FragmentUsersBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class UsersFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentUsersBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentUsersBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        /*binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        }*/
     }
 
     override fun onDestroyView() {
