057de08b54ed898be65e0e59f77f24a5f4718645
ingridmarcelasantosibarra <marcesntos@gmail.com>
2022-07-09 10:36:55 -0500

    Add setup koin

---
+++ app/src/main/java/com/ingridsantos/ceibatechnicaltest/aplication/App.kt
@@ -0,0 +1,20 @@
+package com.ingridsantos.ceibatechnicaltest.aplication
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidContext(this@App)
+            modules()
+        }
+    }
+}
